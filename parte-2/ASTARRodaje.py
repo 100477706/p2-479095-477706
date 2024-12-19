76e5982accabf73c6491a6d272487de0411046a5
Santi <100479095@alumnos.uc3m.es>
2024-12-19 20:26:19 +0100

    Limpiar el codigo de A*

--- parte-2/ASTARRodaje.py
+++ parte-2/ASTARRodaje.py
@@ -10,17 +10,16 @@ import math
 ##### Estructuras de Datos ######
 #################################
 movimientos_posibles = [(-1, 0), (1, 0), (0, -1), (0, 1), (0,0)]
-path_tests = "./parte-2/ASTAR-tests/"
+#ejecutado desde el directorio parte-2
+path_tests = "./ASTAR-tests/"
 
 class Estado:
     def __init__(self,positions, time=0):
+        #nuestro estado consiste en un diccionario con las posiciones de cada avión, el cual se identifica con su indice, y el tiempo
         self.positions = {}
         for i in range(len(positions)):
             self.positions[i] = positions[i]
         self.time = time
-        self.g = 0
-        self.h = 0
-        self.f = 0
 
     def __eq__(self, other):
         """para nuestra implementación solo queremos saber si llegamos
@@ -35,36 +34,9 @@ class Estado:
     def __hash__(self):
         return hash((tuple(sorted(self.positions.items())), self.time))
 
-    def find_adjacent(self, mapa):
-        adjacent = []
-        aviones = []
-        width = len(mapa[0])
-        height = len(mapa)
-        for movimiento in movimientos_posibles:
-            for avion in self.positions.keys():
-                col = self.positions[avion][0] + movimiento[0]
-                row = self.positions[avion][1] + movimiento[1]
-                if self.check_cuadricula(mapa, col, row):
-                    aviones.append((col, row))
-
-    def check_cuadricula(self, mapa, col, row):
-        posible = True
-        width = len(mapa[0])
-        height = len(mapa)
-        if col >= width or row >= height:
-            posible = False
-        if col < 0 or row < 0:
-            posible = False
-        if mapa[col][row].color == 'G':
-            posible = False
-        if mapa[col][row].avion:
-            posible = False
-
-        return posible
-
-
 
 class Cuadricula:
+    #Clase para guardar la cuadrícula
     def __init__(self, color):
         self.color = color
         self.avion = False
@@ -73,7 +45,7 @@ class Cuadricula:
         return str(self.color)
 
 class PriorityQueue(object):
-    """" Nuestra cola contiene la lista [f,estado] por cada estado que se agregue en la lista abierta)"""
+    #Nuestra cola contiene la lista [f,estado] por cada estado que se agregue en la lista abierta)
     def __init__(self):
         self.queue = []
 
@@ -127,22 +99,25 @@ def cargar_mapa(archivo):
     colors = [row for row in data[num_aviones + 1:]]
     #creamos el mapa que será una matriz de instancias Cuadricula
     mapa = []
-    for j in range(len(colors)):
+    for i in range(len(colors)):
         fila = []
-        for i in range(len(colors[0])):
-            cuadricula = Cuadricula(colors[j][i])
+        for j in range(len(colors[0])):
+            cuadricula = Cuadricula(colors[i][j])
             fila.append(cuadricula)
         mapa.append(fila)
     return start, goal, mapa
 
 def movimientos_validos(pos, mapa):
+    #obtenemos los limites del mapa y la x e y del estado
     filas, columnas = len(mapa), len(mapa[0])
     x, y = pos
+    #todas los posibles movimientos en general
     posibles = [(x + dx, y + dy) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]]
+    #comprobamos que sean válidos dentro del mapa
     return [(nx, ny) for nx, ny in posibles if (0 <= nx < filas) and (0 <= ny < columnas) and (mapa[nx][ny].color != 'G')]
 
 def manhattan(estado, goal):
-    #devolvemos la distancia manhattan maxima de los aviones
+    #devolvemos la distancia manhattan maxima entre los aviones
     max = 0
     for i in estado.positions.keys():
         actual = estado.positions[i]
@@ -154,6 +129,7 @@ def manhattan(estado, goal):
     return max
 
 def euclides(estado, goal):
+    # devolvemos la distancia de euclides maxima entre os aviones
     max = 0
     for i in estado.positions.keys():
         actual = estado.positions[i]
@@ -164,6 +140,7 @@ def euclides(estado, goal):
     return max
 
 def hay_conflicto_cruce(posiciones_actuales, posiciones_nuevas):
+    #comprobamos con cada avion que el proximo movimiento no cause ningún conflicto
     for avion1, pos1 in posiciones_actuales.items():
         for avion2, pos2 in posiciones_actuales.items():
             if avion1 != avion2:
@@ -173,7 +150,6 @@ def hay_conflicto_cruce(posiciones_actuales, posiciones_nuevas):
 
 def reconstruir_camino(predecesores, estado_final):
     #Reconstruye el camino desde el estado inicial al objetivo utilizando los predecesores
-
     camino = []
     actual = estado_final
     while actual in predecesores:
@@ -195,21 +171,20 @@ def escritura_camino(path, camino):
                     if pos_inicial == siguiente_pos:
                         f.write(f"({pos_inicial[0]},{pos_inicial[1]}) w ")
                     elif pos_inicial[1] < siguiente_pos[1]:
-                        f.write(f"({pos_inicial[0]},{pos_inicial[1]})  → ") #flecha para izquierda
+                        f.write(f"({pos_inicial[0]},{pos_inicial[1]})  → ") #flecha para derecha
                     elif pos_inicial[1] > siguiente_pos[1]:
-                        f.write(f"({pos_inicial[0]},{pos_inicial[1]})  ← ") #flecha para derecha
+                        f.write(f"({pos_inicial[0]},{pos_inicial[1]})  ← ") #flecha para izquierda
                     elif pos_inicial[0] < siguiente_pos[0]:
                         f.write(f"({pos_inicial[0]},{pos_inicial[1]})  ↓ ") #flecha para abajo
                     elif pos_inicial[0] > siguiente_pos[0]:
                         f.write(f"({pos_inicial[0]},{pos_inicial[1]}) ↑ ") #flecha para arriba
 
-                    #print(pos_inicial, siguiente_pos, pos_inicial[0], siguiente_pos[0], pos_inicial[1], siguiente_pos[1])
-                    #f.write(f"Tiempo {estado.time}: {posiciones}\n")
                 else:
                     f.write(f"({camino[i].positions[avion][0]},{camino[i].positions[avion][1]}) \n")
 
 def escritura_stats(path, time, makespan, h_inicial, expanded):
     with open(path, 'w') as f:
+        #escribimos las estadísticas proporcionadas
         f.write(f"Tiempo total: {time:.2f}s \n")
         f.write(f"Makespan: {makespan} \n")
         f.write(f"h inicial: {h_inicial} \n")
@@ -219,16 +194,20 @@ def escritura_stats(path, time, makespan, h_inicial, expanded):
 ########## Función A* ###########
 #################################
 def ASTAR(start, goal, mapa, heuristica, output_file=None):
-    open_list = PriorityQueue()  # Frontera
+    #creamos la lista abierta, los estados visitados, un diccionario que guarde los costes actuales por cada estado y
+    #un diccionario para los predecedores
+    open_list = PriorityQueue()
     open_list.insert([0, start])
     visitados = set()
     costes = {start: 0}
     predecesores = {}  # Diccionario para rastrear el camino
 
     while not open_list.isEmpty():
+        #sacamos el estado de la lista abierta
         current = open_list.pop()
 
         if current == goal:
+            #encontramos el objetivo
             print("Objetivo conseguido")
             camino = reconstruir_camino(predecesores, current)
             if heuristica == 1:
@@ -239,68 +218,75 @@ def ASTAR(start, goal, mapa, heuristica, output_file=None):
         # Generar todas las combinaciones de movimientos para todos los aviones
         movimientos_aviones = []
         for pos in current.positions.values():
-            movimientos_aviones.append(movimientos_validos(pos, mapa) + [pos])  # Agregamos la  espera
+            movimientos_aviones.append(movimientos_validos(pos, mapa) + [pos])  # Agregamos la  espera con +[pos]
 
         # Crear combinaciones de movimientos paralelos
         for combinacion in product(*movimientos_aviones):  # Todas las combinaciones posibles
             nuevo_positions = {i: combinacion[i] for i in range(len(combinacion))}
 
-            # Validar que no haya colisiones
+            # Comprobamoe que no haya colisiones
             if len(set(nuevo_positions.values())) != len(nuevo_positions):
                 continue
 
-            # Validar conflictos de cruce
+            # Comprobamos que no haya conflictos de cruce
             if hay_conflicto_cruce(current.positions, nuevo_positions):
                 continue
 
-            # Crear el nuevo estado
+            # Creamos el nuevo estado
             nuevo_estado = Estado(nuevo_positions, current.time + 1)
             coste = costes[current] + 1  # Todos los movimientos tienen coste 1
             if nuevo_estado in visitados:
-                if coste < costes[nuevo_estado]:  # Si el nuevo coste es mejor
-                    visitados.remove(nuevo_estado)  # Eliminar de visitados para reexplorarlo
-                else:
-                    continue  # Ignorar si no es un mejor costo
+                if coste > costes[nuevo_estado]:  # Si el nuevo coste es mayor ignoramos al estado y pasamos al siguiente
+                    continue
 
             if nuevo_estado not in costes or coste < costes[nuevo_estado]:
                 costes[nuevo_estado] = coste
-                predecesores[nuevo_estado] = current  # Guardar el predecesor
+                predecesores[nuevo_estado] = current  # Guardamos el predecesor
                 if heuristica == 1:
                     heuristic = manhattan(nuevo_estado, goal)
                 else:
                     heuristic = euclides(nuevo_estado, goal)
                 f = coste + heuristic
+                #si se encuentra en la lista abierta y tiene un mejor coste, lo actualizamos, sino, lo agregamos a la lista abierta
                 if not open_list.actualizar_valor(nuevo_estado, f):
                     open_list.insert([f, nuevo_estado])
 
+        #agregamos el estado a los que ya hemos visitado
         visitados.add(current)
 
     print("No se encontró solución.")
     return None, None, None, None
 
 if __name__ == "__main__":
+    #empezamos el temporizador y cargamos los datos pasados como argumento
     inicio = time.time()
     archivo_mapa = sys.argv[1]
     #para conseguir el nombre del mapa
-    indice_mapa = 22
+    indice_mapa = 14
     nombre_mapa = ""
-    for i in  range(22,len(archivo_mapa)):
+    for i in  range(indice_mapa,len(archivo_mapa)):
         if archivo_mapa[i] != ".":
             nombre_mapa += archivo_mapa[i]
         else:
             break
     num_heuristica = int(sys.argv[2])
+    #cargamos el mapa
     start, goal, mapa = cargar_mapa(archivo_mapa)
+    #hacemos la busqueda
     camino, makespan, h_inicial, expanded = ASTAR(start, goal, mapa, num_heuristica)
+    #no se encontró una solucion
     if camino == None:
         path = path_tests + "mapa-" + str(num_heuristica) + ".output"
         with open(path, 'w') as f:
             f.write("No existe una solución para este problema")
 
     else:
+        #escribimos el fichero outpu
         path_camino = path_tests + nombre_mapa + "-" + str(num_heuristica) + ".output"
         escritura_camino(path_camino, camino)
+        #terminamos el tiempo y calculamos el tiempo total de ejecucion
         fin = time.time()
         tiempo_ejecucion = fin - inicio
+        #pasamos todas las estadisticas y escribimos el fichero stat
         path_stat = path_tests + nombre_mapa + "-" + str(num_heuristica) + ".stat"
         escritura_stats(path_stat, tiempo_ejecucion, makespan, h_inicial, expanded)
